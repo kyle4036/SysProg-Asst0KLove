12556f9b79005a17c67b5c47cbaf3de8c69959bc
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-02-23 15:26:04 -0500

    Update fileSort.c

--- fileSort.c
+++ fileSort.c
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-void* load(char* filename);
+void* load(const char* pathname);
 
 int main(int argc,char* argv){
 
   return 0;
+}
+
+void* load(const char* pathname){
+  int fd = open(pathname, O_RDONLY);
+
+  if(fd == -1){//checks if the file couldn't be opened
+    printf("Cannot Access File: ")
+    switch(errno){
+      case EACCES:  printf("File does not exist!"); return 1;
+      case EOVERFLOW: printf("File is to large!"); return 1;
+      default : printf("Kernal or program error."); return 1;
+    }
+  }
+
+  char temp;
+  read(fd,&temp,sizeof(char))
+  if
 }
