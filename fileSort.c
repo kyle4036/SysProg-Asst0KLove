2b13168d7f4dbc0792269175491cf96c6c9778ca
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-02-23 16:16:42 -0500

    Update fileSort.c

--- fileSort.c
+++ fileSort.c
@@ -7,8 +7,9 @@
 #include <fcntl.h>
 #include <errno.h>
 
-typedef struct llChar_{char data, struct llChar_* next} llChar;
-typedef struct llPntr_{char data, struct llPntr_* next} llPntr;
+typedef struct llChar_{char data; struct llChar_* next} llChar;
+typedef struct llPntr_{char data; struct llPntr_* next} llPntr;
+
 void* load(const char* pathname);
 
 int main(int argc,char* argv){
