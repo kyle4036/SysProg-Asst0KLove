bd90bb808019a7aae1f2ca440f043ad7dca6ae3b
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-02-23 19:13:39 -0500

    Update fileSort.c

--- fileSort.c
+++ fileSort.c
@@ -95,6 +95,9 @@ llChar* pushChar(char c, llChar charList){
   newNode = (llChar*)malloc(sizeof(llChar));
   newNode->data = c;
   newNode->next = &charList;
+  printf("charList address %h\n", charList);
+  printf("newNode address %h\n", newNode);
+
   return newNode;
 }
 //pushPntr()
