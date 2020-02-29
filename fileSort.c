37b06196a13c2821964b9dc1201e252d6e5b5ed9
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-02-29 18:51:47 -0500

    Update fileSort.c

--- fileSort.c
+++ fileSort.c
@@ -26,6 +26,7 @@ void freePntrList(llPntr* data);//Needs to be tested (8)
 
 int charLen(llChar list);//seems to work
 char* cListToString(llChar list);//seems to work
+int cListToInt(llChar list);//seems to work
 void swapPntr(llPntr x, llPntr y);//Needs to be tested (4)
 
 int main(int argc,char* argv){
