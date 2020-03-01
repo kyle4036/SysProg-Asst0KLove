450f3e7aff8f802611fd3ad9dd95837cdee27d38
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-03-01 16:09:01 -0500

    Update fileSort.c

--- fileSort.c
+++ fileSort.c
@@ -78,8 +78,8 @@ int main(int argc,char* argv[]){
   //printf("address of the 3rd llChar - %0x\n",getPntr(*pHead, 3));
   llPntr element3 = getPntr(*pHead,3);
   llPntr element6 = getPntr(*pHead,6);
-  printf("*element 3 - %0x \n*element 6 - %0x \n", element3,element6);
-  swapPntr(element3, element6);
+  printf("*element 3 - %0x \n*element 6 - %0x \n", &element3,&element6);
+  swapPntr(&element3, &element6);
   //Doing this thing
 
   printf("elements after swap- \n");
