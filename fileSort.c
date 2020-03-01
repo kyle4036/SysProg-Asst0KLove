dc0f318d2b2f4fb65fe63bada745c0723486d615
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-03-01 10:36:36 -0500

    Update fileSort.c

--- fileSort.c
+++ fileSort.c
@@ -53,7 +53,7 @@ int main(int argc,char* argv){
   int i;
   for(i = 0;i < 10;i++){
     pushChar(i+'a', &charHead);
-    pushPntr(&charHead, &pHead);
+    //pushPntr(&charHead, &pHead);
     charHead = &cElement0;
   }
 
