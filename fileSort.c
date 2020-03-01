2ccd501204d2d75ecdf4a82a6354956b702ee686
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-03-01 18:37:55 -0500

    Update fileSort.c

--- fileSort.c
+++ fileSort.c
@@ -76,7 +76,7 @@ int main(int argc,char* argv[]){
   //test stuff here
 
   //printf("address of the 3rd llChar - %0x\n",getPntr(*pHead, 3));
-  llPntr* element3 = &getPntr(*pHead,3);
+  llPntr* element3 = &(getPntr(*pHead,3));
   /*llPntr element6 = getPntr(*pHead,6);
   printf("*element 3 - %0x \n*element 6 - %0x \n", element3,element6);
 
@@ -88,7 +88,7 @@ int main(int argc,char* argv[]){
   element3.data = element6.data;
   element6.data = temp;*/
 
-  *element3.data = &cElement0;
+  (*element3).data = &cElement0;
 
   //Doing this thing
 
