69e206d8dd7ce97ed29c59b6e29a39a09e05e163
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-03-01 18:13:00 -0500

    Update fileSort.c

--- fileSort.c
+++ fileSort.c
@@ -84,9 +84,9 @@ int main(int argc,char* argv[]){
   printf("elements after first swap- \n");
   printPntrList(*pHead);
 
-  void* temp = *element3.data;
-  element3.data = *element6.data;
-  element6.data = temp;
+  void* temp = element3.data;
+  *(element3.data) = element6.data;
+  *(element6.data) = temp;
 
   //Doing this thing
 
